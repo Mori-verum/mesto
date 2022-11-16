b1da870e787ec5933a3c47f841b2d85928f8f1cb
Selivokhina Anastasia <nastya.selivokhina8@gmail.com>
2022-11-16 13:56:02 +0300

    feat: валидация форм

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,17 @@
+const selectors = {
+  formSectionSelector: '.popup__form-section',
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  inputErrorSelector: '.popup__input-error',
+  inputErrorActiveClass: 'popup__input-error_active',
+  submitSelector: '.popup__submit',
+  inactiveSubmitClass: 'popup__submit_inactive'
+}
 
 const checkInputValidity = (inputElem) => {
   const isValid = inputElem.validity.valid;
-  const formSection = inputElem.closest('.popup__form-section');
-  const errorElement = formSection.querySelector('.popup__input-error');
+  const formSection = inputElem.closest(selectors.formSectionSelector);
+  const errorElement = formSection.querySelector(selectors.inputErrorSelector);
 
   if (isValid) {
     hideInputError(errorElement);
@@ -13,12 +22,12 @@ const checkInputValidity = (inputElem) => {
 
 const showInputError = (errorElem, errorMessage) => {
   errorElem.textContent = errorMessage;
-  errorElem.classList.add('popup__input-error_active');
+  errorElem.classList.add(selectors.inputErrorActiveClass);
 }
 
 const hideInputError = (errorElem) => {
   errorElem.textContent = '';
-  errorElem.classList.remove('popup__input-error_active');
+  errorElem.classList.remove(selectors.inputErrorActiveClass);
 }
 
 const toggleSubmitStatus = (inputList, submitBtn) => {
@@ -26,26 +35,38 @@ const toggleSubmitStatus = (inputList, submitBtn) => {
 
   if (hasInvalidInput) {
     submitBtn.setAttribute('disabled', true);
-    submitBtn.classList.add('popup__submit_inactive');
+    submitBtn.classList.add(selectors.inactiveSubmitClass);
   } else {
     submitBtn.removeAttribute('disabled');
-    submitBtn.classList.remove('popup__submit_inactive');
+    submitBtn.classList.remove(selectors.inactiveSubmitClass);
   }
 }
 
 
-const form = document.querySelector('.popup__form');
+const setInputListener = (formElem) => {
+  const inputList = Array.from(formElem.querySelectorAll(selectors.inputSelector));
+  const submitBtn = formElem.querySelector(selectors.submitSelector);
 
-const inputList = Array.from(form.querySelectorAll('.popup__input'));
-const submitBtn = form.querySelector('.popup__submit');
+  toggleSubmitStatus(inputList, submitBtn);
 
-toggleSubmitStatus(inputList, submitBtn);
-
-inputList.forEach((inputElem) => {
-  inputElem.addEventListener('input', () => {
-    checkInputValidity(inputElem);
-    toggleSubmitStatus(inputList, submitBtn);
+  inputList.forEach((inputElem) => {
+    inputElem.addEventListener('input', () => {
+      checkInputValidity(inputElem);
+      toggleSubmitStatus(inputList, submitBtn);
+    })
   })
-})
+}
+
+
+const enableValidation = (selectors) => {
+
+  const formList = document.querySelectorAll(selectors.formSelector);
+
+  formList.forEach((form) => {
+    setInputListener(form);
+  })
+}
+
+enableValidation(selectors);
 
 
